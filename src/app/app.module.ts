66052798ad73207cfbe5bf39f5c1a65a2d1f0ec4
OliveiraOtavio <otavio.oliveira@msn.com>
2023-01-02 22:20:59 -0300

    Configuracion para Edicion componente Experiencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +33,9 @@ import { interceptorProvider } from './service/interceptor-service';
     ProyectosComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent
   ],
   imports: [
     BrowserModule,
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
